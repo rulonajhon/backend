ccd2322289bfcb6882bb0cf6c27572b72fb5055b
rulonajhon <118814124+rulonajhon@users.noreply.github.com>
2024-05-15 02:22:53 +0800

    Delete backend3 directory

--- backend3/models/Admins.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-class Admins(BaseModel):
-    AdminID: int
-    Username: str
-    Password:str 
--- backend3/models/Students.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class Students(BaseModel):
-    StudentID: int
-    Username: str
-    Password: str
-    Name: str
-    Email: str
